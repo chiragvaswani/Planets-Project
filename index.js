6bb42632724d5864a7b036766197c9e764e95925
chiragvaswani <chiragvaswani751@gmail.com>
2021-08-09 17:08:32 +0530

    Define a function to filter planets and store them in the array

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const parse = require('csv-parse')
 const fs = require('fs')
 
-const results = [];
+const habitablePlanets = [];
+
+const isHabitable = planet => planet['koi_disposition'] === 'CONFIRMED' && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11 && planet['koi_prad'] < 1.6;
 
 fs.createReadStream('kepler_data.csv')
     .pipe(parse({
@@ -10,12 +12,13 @@ fs.createReadStream('kepler_data.csv')
 
     })) // pipe function connects a readable stream source to a writable stream destination
     .on('data', (data) => {
-        results.push(data);
+        if (isHabitable(data))
+            habitablePlanets.push(data);
     })
     .on('error', (err) => {
         console.log(err);
     })
     .on('end', () => {
-        console.log(results)
-        console.log('done')
+        console.log(habitablePlanets.map(planet => planet['kepler_name']))
+        console.log(`${habitablePlanets.length} habitable planets found.`)
     })
